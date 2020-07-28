72af6ee5f245c0dcd904da3fc36bebb72f158196
sahsx <605260097@qq.com>
2020-07-28 09:54:06 +0800

    add uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,22 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+
+let plugins = [
+  commonjs(),
+  nodeResolve(),
+  typescript({ target: 'es5' }),
+  babel({
+    babelHelpers: 'runtime',
+    exclude: ['node_modules'],
+    extensions: ['.ts', '.tsx']
+  })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = plugins.concat(uglify())
+}
 
 export default {
   input: 'src/main.ts',
@@ -10,14 +26,5 @@ export default {
     format: 'umd'
   },
   external: [/@babel\/runtime/],
-  plugins: [
-    commonjs(),
-    nodeResolve(),
-    typescript({ target: 'es5' }),
-    babel({
-      babelHelpers: 'runtime',
-      exclude: ['node_modules'],
-      extensions: ['.ts', '.tsx']
-    })
-  ]
+  plugins
 }
